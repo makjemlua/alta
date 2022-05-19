f5dbc56881360c946fa66bbc07ae0c5c2e01c06f
Nguyen An <nguyen.van.an>
2022-05-19 17:07:43 +0700

    edit layout banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,18 +12,13 @@
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar elevation-4">
-    <!-- Brand Logo -->
-    <a href="" class="brand-link">
-      <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Trang quản trị</span>
-    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('img/logo.png')}}" class="img-circle elevation-2" alt="User Image" style="width: 20rem; border-radius: unset; box-shadow: unset !important;">
         </div>
         <div class="info">
           {{-- <a href="#" class="d-block">{{Auth::user()->name}}</a> --}}
